efe5282d421f50651f2985a6415d341ffef86718
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-08-23 14:05:01 +0200

    add signup and signin component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDeatils from "./components/projects/ProjectDeatils";
-
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDeatils} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
