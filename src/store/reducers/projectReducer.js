fa5036c9fde0f667358b80fbfbc0f1255b706ceb
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-08-26 17:10:41 +0200

    connected app to redux store, pass example data to dashboard/projectsList

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,14 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "someone help me", content: "to be a senior" },
+    {
+      id: "2",
+      title: "do you know that joke?",
+      content: "every man in Mexico is senior"
+    },
+    { id: "3", title: "Good one", content: "heheheheh" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
